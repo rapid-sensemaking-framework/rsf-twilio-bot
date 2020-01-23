b01b1628958022acad9bf866d16c6979ea272e8d
Connor Turland <connorturland@gmail.com>
2020-01-22 18:51:22 -0800

    remove raw js files

--- protocol.d.ts
+++
@@ -1,7 +0,0 @@
-declare const RECEIVE_MESSAGE = "receive_message";
-declare const SEND_MESSAGE = "send_message";
-interface TwilioMessage {
-    number: string;
-    message: string;
-}
-export { RECEIVE_MESSAGE, SEND_MESSAGE, TwilioMessage };
--- protocol.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var RECEIVE_MESSAGE = 'receive_message';
-exports.RECEIVE_MESSAGE = RECEIVE_MESSAGE;
-var SEND_MESSAGE = 'send_message';
-exports.SEND_MESSAGE = SEND_MESSAGE;
